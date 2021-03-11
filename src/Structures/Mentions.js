3af2a171255e05228fa1b0a70365fc45f04ee3b6
Bryzzen <xdbrgames10@gmail.com>
2021-03-11 08:39:11 -0300

    v1

--- src/Structures/Mentions.js
+++ src/Structures/Mentions.js
@@ -4,15 +4,16 @@ const User = require("./User")
 
 module.exports = class Mentions {
     constructor(client, payload){
+        //console.log(payload)
         this.roles = new Chest(Role)
-        if(payload.mention_roles.length > 0){
+        if(payload.mention_roles){
         payload.mention_roles.forEach(e => {
             this.roles.set(client.roles.get(e).id, client.roles.get(e))
         })
         }
 
         this.users = new Chest(User)
-        if(payload.mentions.length > 0){
+        if(payload.mentions){
         payload.mentions.forEach(e => {
             this.users.set(client.users.get(e.id).id, client.users.get(e.id))
         })
