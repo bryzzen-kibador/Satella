cf71a914d9de4d8e8887edb3dad6ed9f1c92cce9
Bryzzen <xdbrgames10@gmail.com>
2021-03-11 10:23:25 -0300

    v1

--- src/Handler/MESSAGE_UPDATE.js
+++ src/Handler/MESSAGE_UPDATE.js
@@ -3,7 +3,7 @@ const Message = require('../Structures/Message');
 module.exports = async (client, payload) => {
   const channel = client.channels.get(payload.d.channel_id);
   const message = channel.messages.get(payload.d.id);
-  channel.messages.remove(message.id);
+  channel.messages.remove(payload.d.id);
   channel.messages.set(payload.d.id, new Message(client, payload.d));
 
   const messageNew = channel.messages.get(payload.d.id);
